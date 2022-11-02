bf59fdc4535d5f98c275167a3518f0166e092474
Shlok Sabarwal <shlok190303@gmail.com>
2022-11-02 13:13:14 -0500

    Fixing numpy detection for windows deployment

--- contrib/packaging-python/conda/script.ps1
+++ contrib/packaging-python/conda/script.ps1
@@ -1,1 +1,7 @@
-Get-ChildItem -Directory "C:\Users\builder\miniconda3\envs\build-env\pkgs\numpy-base*" | Rename-Item -NewName numpy-base
+$pyver = python --version
+if($pyver -match '3.9' -or $pyver -match '3.10'){
+    Get-ChildItem -Directory "C:\Users\builder\miniconda3\envs\build-env\pkgs\numpy-base*" | Rename-Item -NewName numpy-base
+}
+else{
+    Get-ChildItem -Directory "C:\Users\builder\miniconda3\envs\build-env\pkgs\numpy*" | Rename-Item -NewName numpy-base
+}
