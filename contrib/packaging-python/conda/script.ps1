153c6bb7cb399f146c9bd7bb5d7458264337c5d3
Shlok Sabarwal <shlok190303@gmail.com>
2022-11-03 14:37:34 -0500

    Merged branch feature/ci/linux-deployment into feature/ci/staging

--- contrib/packaging-python/conda/script.ps1
+++ contrib/packaging-python/conda/script.ps1
@@ -1,7 +1,2 @@
 $pyver = python --version
-if($pyver -match '3.9' -or $pyver -match '3.10'){
-    Get-ChildItem -Directory "C:\Users\builder\miniconda3\envs\build-env\pkgs\numpy-base*" | Rename-Item -NewName numpy-base
-}
-else{
-    Get-ChildItem -Directory "C:\Users\builder\miniconda3\envs\build-env\pkgs\numpy*" | Rename-Item -NewName numpy-base
-}
+Get-ChildItem -Directory "C:\Users\builder\miniconda3\envs\build-env\pkgs\numpy*" | Rename-Item -NewName numpy-base
